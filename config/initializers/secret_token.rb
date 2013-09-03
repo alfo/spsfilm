1230df8561ffebd99eb8e90cc6d94d2d9f7acb7b
Alex Forey <me@alexforey.com>
2013-09-03 18:28:15 +0100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Spsfilm::Application.config.secret_key_base = '98fd28f9c37dae11d73aca9db57a6b9d15110811ba9111a771920717da3dfd2082bdf63dcac48bd22e55eb8f22c706663c0335cbdd375cb8a71a281cea16f253'
